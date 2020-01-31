cd42973c08d7ea4ead76c77bf346cc33fb8cd4c7
Gero Müller <post@geromueller.de>
2020-01-31 23:49:48 +0100

    add musicians scripts

---
+++ Assets/_scripts/Singer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singer : Musician
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        this.gameObject.transform.Translate(Time.fixedDeltaTime, 0, 0);
+    }
+}
+

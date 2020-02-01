4973474a954dec2bb42c612ee5599b447d273332
aleurs <andreas.leurs@gmx.de>
2020-02-01 14:51:01 +0100

    New camera + fonts and main menu

---
+++ Assets/_scripts/ButtonAbout.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAbout : MonoBehaviour
+{
+    public Transform startPosition;
+    public Transform positionTeam;
+    public GameObject mainMenu;
+
+    private void OnMouseDown()
+    {
+        StartCoroutine(MoveToPosition(mainMenu, positionTeam.position, 1f));
+    }
+
+    public IEnumerator MoveToPosition(GameObject _mainMenu, Vector3 position, float timeToMove)
+    {
+        var currentPos = mainMenu.transform.position;
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            mainMenu.transform.position = Vector3.Lerp(currentPos, -position, t);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/_scripts/ButtonStart.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonStart : MonoBehaviour
+{
+    public GameObject camStart, camEnd;
+
+    public void Start()
+    {
+        GameObject.Find("Canvas/BlackScreen").GetComponent<Image>().color = new Color(0, 0, 0, 0);
+    }
+
+    private void OnMouseDown()
+    {
+        camStart.SetActive(false);
+        camEnd.SetActive(true);
+
+        StartCoroutine(LoadLevel());
+        StartCoroutine(ScreenFadeOut());
+    }
+
+    public IEnumerator LoadLevel()
+    {
+        // Wait for one second
+        yield return new WaitForSeconds(2f);
+        
+        SceneManager.LoadScene("levelCamera2");
+    }
+
+    public IEnumerator ScreenFadeOut()
+    {
+        yield return new WaitForSeconds(1f);
+
+        float x = 0;
+        while(x < 1)
+        {
+            Color color = GameObject.Find("Canvas/BlackScreen").GetComponent<Image>().color;
+            x += 4f / 256f;
+            color.a = x;
+            GameObject.Find("Canvas/BlackScreen").GetComponent<Image>().color = color;
+            Debug.Log("color.a " + color.a);
+            yield return new WaitForSeconds(1f / 256f);
+        }
+    }
+}

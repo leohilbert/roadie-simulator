4ca1d4af4a8cf5c35ac3475fb27b0d9e349854c9
aleurs <andreas.leurs@gmx.de>
2020-02-01 17:30:35 +0100

    Anfang camera movement

--- Assets/_scripts/CameraControl.cs
+++ Assets/_scripts/CameraControl.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    public GameObject camNormal, camshakesmall, camshakemedium, camshakestrong;
+    [Header("Cameras")]
+    public GameObject camNormal;
+    public GameObject camshakesmall;
+    public GameObject camshakemedium;
+    public GameObject camshakestrong;
 
+    [Header("Player")]
+    public GameObject player1;
+    public GameObject player2;
+
+    [Header("Boxes")]
+    public GameObject boxStage;
+    public GameObject boxCrowd;
+
+    private Transform mainTransform;
+    Vector3 camPosStage = new Vector3(12.8f, 2.1f, -14.5f);
+    Quaternion camRotStage = Quaternion.Euler(-3.64f, 0, 0);
+
+    Vector3 camPosCrowd = new Vector3(12.2f, -1.2f, -0.6f);
+    Quaternion camRotCrowd = Quaternion.Euler(-18.85f, 0, 0);
+
+    [Header("ShakeLevel")]
     [SerializeField] ShakeLevel shakeLevel = ShakeLevel.none;
-    
+
+    private void Start()
+    {
+        mainTransform = gameObject.transform;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -39,9 +64,25 @@ public class CameraControl : MonoBehaviour
                 break;
             default:
                 break;
-
         }
+
+        if (player1.GetComponent<PlayerParams>().OnStage() && player2.GetComponent<PlayerParams>().OnStage())
+        {
+            Vector3.MoveTowards(gameObject.transform.position, camPosStage, 0.1f);
+        }
+        else if ((player1.GetComponent<PlayerParams>().OnStage() && player2.GetComponent<PlayerParams>().InCrowd()) ||
+            player1.GetComponent<PlayerParams>().InCrowd() && player2.GetComponent<PlayerParams>().OnStage())
+        {
+            Vector3.MoveTowards(gameObject.transform.position, mainTransform.position, 0.1f);
+        }
+        else if(player1.GetComponent<PlayerParams>().InCrowd() && player2.GetComponent<PlayerParams>().InCrowd())
+        {
+            Vector3.MoveTowards(gameObject.transform.position, camPosCrowd, 0.1f);
+        }
+
     }
+
+
 
     public void ChangeShakeLevel(ShakeLevel _shakeLevel)
     {
---
+++ Assets/_scripts/PlayerParams.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerParams : MonoBehaviour
+{
+    bool on_stage = false;
+    bool in_crowd = false;
+
+    public GameObject camObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "BoxStage")
+        {
+            SetOnStage(true);
+        }
+
+        if(other.tag == "BoxCrowd")
+        {
+            SetInCrowd(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "BoxStage")
+        {
+            SetOnStage(false);
+        }
+
+        if(other.tag == "BoxCrowd")
+        {
+            SetInCrowd(false);
+        }
+    }
+
+    public bool OnStage()
+    {
+        return on_stage;
+    }
+
+    public bool InCrowd()
+    {
+        return in_crowd;
+    } 
+
+    public void SetOnStage(bool _onStage)
+    {
+        on_stage = _onStage;
+        Debug.Log(gameObject.name + " on stage: " + _onStage);
+    }
+
+    public void SetInCrowd(bool _inCrowd)
+    {
+        in_crowd = _inCrowd;
+        Debug.Log(gameObject.name + " in crowd: " + _inCrowd);
+    }
+}
